63dc86582e5b0667dc44bff328b216e8c44a3a0c
Nagaraj <santnagaraj@deloitte.com>
2016-11-09 22:01:58 +0530

    Adding templates

--- admin/src/app/main/access/access.controller.js
+++ admin/src/app/main/access/access.controller.js
@@ -8,5 +8,53 @@
       /** @ngInject */
       function AccessController(AccessData) {
         var vm = this;
+
+        vm.superUserRequest = {
+          'count': 2,
+          'list': [{
+            'face': '',
+            'name': 'John Doe',
+            'userID': 1
+          }, {
+            'face': '',
+            'name': 'John Doe',
+            'userID': 2
+          }]
+        };
+
+        vm.adminRequest = {
+          'count': 3,
+          'list': [{
+            'face': '',
+            'name': 'John Doe',
+            'userID': 1
+          }, {
+            'face': '',
+            'name': 'John Doe',
+            'userID': 2
+          }, {
+            'face': '',
+            'name': 'John Doe',
+            'userID': 3
+          }]
+        };
+
+        vm.registered = {
+          title: 'Registered Users',
+          count: 30
+        };
+
+        vm.superusers = {
+          title: 'Super Users',
+          count: 20
+        };
+
+        vm.admins = {
+          title: 'Admins',
+          count: 3
+        }
+        vm.approveRequest = function(user) {
+
+        };
       }
 })();
