249a1c8b712251e9f04170c4045beccbd432bcde
Nagaraj <santnagaraj@deloitte.com>
2016-11-29 21:19:13 +0530

    Changes to landing page for integrating API

--- main/src/app/landing/landing.controller.js
+++ main/src/app/landing/landing.controller.js
@@ -20,9 +20,11 @@
      */
     RestService.get('courses/popular')
       .then(function(response) {
-        if (response.data || response.data.courseList.length > 0) {
+        if (response.data && response.data.courses.length > 0) {
           vm.isPopularCoursesAvailable = true;
-          vm.popularCourses = response.data.courseList;
+          vm.popularCourses = response.data.courses;
+        } else {
+          $log.error('No popular courses found');
         }
       }, function(err) {
         $log.error(err);
@@ -34,9 +36,11 @@
      */
     RestService.get('activities/upcoming')
       .then(function(response) {
-        if (response.data || response.data.activityList.length > 0) {
+        if (response.data && response.data.activities.length > 0) {
           vm.isUpcomingActivitiesAvailable = true;
-          vm.response.data.activityList = response.data.activityList
+          vm.response.data.activityList = response.data.activities;
+        } else {
+          $log.error('No upcoming activities found');
         }
       }, function(err) {
         $log.error(err);
